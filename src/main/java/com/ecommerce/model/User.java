282f44eafc230f60b3cd9303b2bc996c15e231df
Anas Khaled <anaskhaled23.9.4@gmail.com>
2026-05-29 05:42:22 +0300

    Added User + Linked to products,addresses,roles

---
+++ src/main/java/com/ecommerce/model/User.java
@@ -0,0 +1,71 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username") ,
+                @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 17 , message = "Username cannot exceed 17 characters")
+    @Column(name = "username")
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "password")
+    private String password;
+
+    @NotBlank
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    @ManyToMany(fetch = FetchType.EAGER,
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private Set<Product> products = new HashSet<>();
+
+    @OneToMany(mappedBy = "user",
+    cascade = CascadeType.ALL , orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+
+}
