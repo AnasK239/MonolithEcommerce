4e9209e4e48b36ce183b9b88144ccb1f9df31dad
Anas Khaled <anaskhaled23.9.4@gmail.com>
2026-05-29 05:41:19 +0300

    Added Roles and linked to user

---
+++ src/main/java/com/ecommerce/model/Role.java
@@ -0,0 +1,28 @@
+package com.ecommerce.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @ToString.Exclude
+    @Enumerated(EnumType.STRING) // beacuse enums default to persisting as integers
+    @Column(name = "role_name")
+    private AppRoles roleName;
+
+
+}
