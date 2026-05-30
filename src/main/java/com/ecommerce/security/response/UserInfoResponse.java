fccfcb6a7ded41240965400833d6c17cbf412d3f
Anas Khaled <anaskhaled23.9.4@gmail.com>
2026-05-30 10:36:17 +0300

    Security response DTOs

---
+++ src/main/java/com/ecommerce/security/response/UserInfoResponse.java
@@ -0,0 +1,27 @@
+package com.ecommerce.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+public class UserInfoResponse {
+    private Long id;
+    private String jwtToken;
+    private String username;
+    private List<String> roles;
+
+    public UserInfoResponse(Long id,String jwtToken ,String username, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+        this.jwtToken = jwtToken;
+    }
+
+
+}
+
+
