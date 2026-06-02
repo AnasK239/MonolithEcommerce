d0236888f4d5d5efa59c72d6174fd58c629b8c72
Anas Khaled <anaskhaled23.9.4@gmail.com>
2026-06-02 08:27:00 +0300

    jwt auth modifications

--- src/main/java/com/ecommerce/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/security/jwt/JwtUtils.java
@@ -1,17 +1,21 @@
 package com.ecommerce.security.jwt;
 
+import com.ecommerce.security.services.UserDetailsImpl;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
@@ -26,14 +30,34 @@ public class JwtUtils {
 
     @Value("${spring.app.jwtSecret}")
     private String jwtSecret;
-    // Getting JWT from Header
-    public String getJwtFromHeader(HttpServletRequest request){
-        String bearerToken = request.getHeader("Authorization");
-        logger.debug("Authorization header: {}" , bearerToken);
-        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
-            return bearerToken.substring(7);
+
+    @Value("${spring.app.jwtCookies}")
+    private String jwtCookie;
+
+    public String getJwtFromCookies(HttpServletRequest request) {
+        Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+        if (cookie != null) {
+            return cookie.getValue();
         }
-        return null;
+        else return null;
+    }
+
+    public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
+        String jwt = generateJwtToken(userPrincipal);
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie , jwt)
+                .path("/api")
+                .maxAge(24*60*60)
+                .httpOnly(false)
+                .build();
+        return cookie;
+    }
+
+
+    public ResponseCookie getClearedCookie() {
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie , null)
+                .path("/api")
+                .build();
+        return cookie;
     }
 
     // Generate token from username
@@ -82,5 +106,6 @@ public class JwtUtils {
         }
         return false;
     }
+
 }
 
