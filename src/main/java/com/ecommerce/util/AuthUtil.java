2311697fdc3117e4824d0b993f0ae6aa153bbdcb
Anas Khaled <anaskhaled23.9.4@gmail.com>
2026-06-02 08:20:27 +0300

    Authentication details helper

---
+++ src/main/java/com/ecommerce/util/AuthUtil.java
@@ -0,0 +1,37 @@
+package com.ecommerce.util;
+
+import com.ecommerce.model.User;
+import com.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtil {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public String loggedInEmail(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUsername(auth.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return user.getEmail();
+    }
+
+    public Long loggedInUserId() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUsername(auth.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return user.getUserId();
+    }
+
+    public User loggedInUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUsername(auth.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return user;
+    }
+}
